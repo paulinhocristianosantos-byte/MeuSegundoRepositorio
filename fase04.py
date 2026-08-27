26e27ad220fe6c5d7cf2a848079ab02979988786
Paulo Sântos <paulinhocristianosantos@gmail.com>
2026-08-27 19:46:13 -0300

    Commit teste 27
    commit para função pull

---
+++ fase04.py
@@ -0,0 +1,9 @@
+idade = int(input("Digite sua idade:"))
+altura = float(input("Digite sua altura:"))
+
+if idade >= 10 and altura >= 1.40:
+    print("Entrada permitida!")
+else:
+   print("Entrada nao permitida!")
+
+

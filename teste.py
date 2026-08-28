a2a94c98a2cf072606acb52ec7f7706c9977f544
paulinhocristianosantos-byte <paulinhocristianosantos@gmail.com>
2026-08-27 21:08:34 -0300

    commit teste do professor

--- teste.py
+++ teste.py
@@ -1,5 +1,8 @@
 print("Hello World")
 peso = float(input("Digite seu peso:"))
 altura = float(input("Digite sua altura:"))
-imc = peso * altura 
+if altura >1.80:
+    print("Voce foi selecionado pela agencia")
 
+
+ola :) 
